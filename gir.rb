4b93aaf1efba1c0092c4e5d608fd9b468c2b18d9
Danyl S. Nysom <danyl.nysom@gmail.com>
2015-02-01 15:06:11 -0800

    Dylan-style tidying

--- gir.rb
+++ gir.rb
@@ -2,71 +2,59 @@
 
 require 'octokit'
 
-usage = "Usage: \truby gir.rb <repo.txt> <organization> <github username> <github password>\nex)\truby gir.rb repo.txt UBC username password\n\n"
+usage = "Usage: ruby gir.rb <repo file> <organization> <github username> <github password>"
 
 #setup user login
-if ARGV.length == 4
-	puts "logging in"
-	client = Octokit::Client.new \
-		:login =>ARGV[2],
-		:password =>ARGV[3]
-	user = client.user
-	user.login
+if ARGV.length >= 4
+        puts "logging in"
+        client = Octokit::Client.new \
+                :login =>ARGV[2],
+                :password =>ARGV[3]
+        user = client.user
+        user.login
+elsif ARGV.length >= 2
+        puts "No GitHub account specified - using limited requester"
+        client = Octokit
 else
-	puts "Insufficiant login info using limited requester"
-	puts usage
-	client = Octokit
+        abort(usage)
 end
-#determine repo
-if ARGV.length < 4
-	die (usage)
-else
-	puts "using repo file:\t" + ARGV[0]
-	puts "Scanning organization:\t" + ARGV[1]
-	org = ARGV[1]
-end
+
+puts "using repo file:\t" + ARGV[0]
+puts "Scanning organization:\t" + ARGV[1]
+org = ARGV[1]
 
 text=File.open(ARGV[0]).read
 text.gsub!(/\r\n?/,"\n")
 puts "Scraping Issues...\n"
 text.each_line do |line|
-	puts line
-	line.gsub!(/\n/,"")
-	output = File.open("#{line}"+".md","w")
-	repository = org+"/"+line
-	client.auto_paginate = true;
-	issues = client.issues repository
-	issues.each do|issue|
-		output << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
-		output << "Title:\t"
-		output << issue.title
-		output << "\n"
-		
-		output << "Author:\t"
-		output << issue.user.login
-		output << "\n"
-		if issue.assignee 
-			output << "Assignee:\t"
-			output << issue.assignee.login
-			output << "\n"
-		end
+        puts line
+        line.gsub!(/\n/,"")
+        output = File.open("#{line}"+".md","w")
+        repository = org+"/"+line
+        client.auto_paginate = true;
+        issues = client.issues(repository)
+        issues.each do|issue|
+                output << "#"
+                output << issue.title
+                
+                output << "\n**Author:** " << issue.user.login
+                output << "\n**Assignee:** "
+                if issue.assignee 
+                    output << issue.assignee.login
+                end
 
-		issue.labels.each do |label|
-			output << "Lable:\t"
-			output << label.name
-			output << "\n"
-		end
-		output << "Body:\n"
-		output << issue.body
-		output << "\n"
-		output << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n"
-		
-		sleep(1);
+                output << "\n**Labels:** "
+                issue.labels.each do |label|
+                    output << label.name << " "
+                end
+                output << "\n" << issue.body.gsub!(/\r\n?/,"\n") << "\n------\n"
+                
+                sleep(1);
 
-	end
-	output.close
-end	
-	
+        end
+        output.close
+end     
+        
 
 #get File Commits
 #filename
